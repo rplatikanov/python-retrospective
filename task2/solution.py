81bfe97adc0c76f4965b21eaec6c83760916d23c
Radoslav Platikanov <rplatikanov@gmail.com>
2013-04-28 19:14:00 +0300

    task2: use nested function instead of class for cache

--- task2/solution.py
+++ task2/solution.py
@@ -23,26 +23,22 @@ def zip_with(func, *iterables):
     return map(lambda zipped: func(*zipped), zip(*iterables))
 
 
-class CachedFunc:
-    def __init__(self, func, cache_size):
-        self.func = func
-        self.cache_size = cache_size
-        self.cache = OrderedDict()
-
-    def __call__(self, *args):
-        arg_key = tuple(args)
-        if arg_key in self.cache:
-            return self.cache[arg_key]
-
-        if len(self.cache) == self.cache_size:
-            self.cache.popitem(False)
-
-        result = self.func(*args)
-        self.cache[arg_key] = result
-        return result
-
-
 def cache(func, cache_size):
     if cache_size <= 0:
         return func
-    return CachedFunc(func, cache_size)
+
+    cache = OrderedDict()
+
+    def cached(*args):
+        arg_key = tuple(args)
+        if arg_key in cache:
+            return cache[arg_key]
+
+        if len(cache) == cache_size:
+            cache.popitem(False)
+
+        result = func(*args)
+        cache[arg_key] = result
+        return result
+
+    return cached
